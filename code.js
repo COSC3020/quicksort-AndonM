7e587c3521dc7a5aef15ca0816092d231ae98fd1
Andon Mefford <andonmefford@outlook.com>
2023-09-26 22:19:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function quicksort(array) {
-    return array;
+    const sortedArray = [];
+    const tempArray = [];
+    tempArray.push(array);
+  
+    while (tempArray.length > 0) {
+      const currentArray = tempArray.pop();
+  
+      if (currentArray.length <= 1) {
+        sortedArray.push(...currentArray);
+      } else {
+        const pivotIndex = Math.floor(currentArray.length / 2);
+        const pivot = currentArray[pivotIndex];
+        const left = [];
+        const right = [];
+  
+        for (let i = 0; i < currentArray.length; i++) {
+          if (i === pivotIndex) {
+            continue;
+          }
+          if (currentArray[i] < pivot) {
+            left.push(currentArray[i]);
+          } else {
+            right.push(currentArray[i]);
+          }
+        }
+        tempArray.push(right);
+        tempArray.push([pivot]);
+        tempArray.push(left);
+      }
+    }
+    return sortedArray;
 }
